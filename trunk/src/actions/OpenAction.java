2744109f3f5ceb79bc6ddbf5f99428daa7857b57
mschaeuble@swissonline.ch <mschaeuble@swissonline.ch@a0df532e-5e5b-11de-8a4b-256bef446c6c>
2009-08-24 20:12:54 +0000

    Added InformativeTextPainter and implemented loading message. Small cleanup in main class

--- trunk/src/actions/OpenAction.java
+++ trunk/src/actions/OpenAction.java
@@ -11,12 +11,15 @@ import java.util.Set;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
 import javax.swing.filechooser.FileFilter;
 
+import org.jdesktop.swingx.JXMapViewer;
 import org.jdesktop.swingx.mapviewer.GeoPosition;
+import org.jdesktop.swingx.painter.Painter;
 
 import painters.GPXPainter;
-
+import painters.InformativeTextPainter;
 import data.Bounds;
 import data.coordinate.LatLon;
 import data.gpx.GPXData;
@@ -26,6 +29,51 @@ public class OpenAction extends ApplicationAction
 {
 	private static final long serialVersionUID = 1L;
 	private MapComponent mapComponent;
+	
+	private class OpenActivity extends SwingWorker<Object, Object> {
+		private File file;
+		private Painter<JXMapViewer> infoTextOverlay;
+		
+		public OpenActivity(File file, Painter<JXMapViewer> infoTextOverlay)
+		{
+			this.file = file;
+			this.infoTextOverlay = infoTextOverlay;
+		}
+		
+		@Override
+		protected Object doInBackground() throws Exception {
+			GPXReader reader;
+			try
+			{
+				reader = new GPXReader(file);
+				GPXData data = reader.getGPXDataObject();
+				mapComponent.addOverlayPainter(new GPXPainter(data).getPainter());
+				// TODO: Remove debug information
+				System.out.println(data.toString());
+				
+				Bounds bounds = data.getBounds();
+				LatLon center = bounds.getCenter();
+				mapComponent.setCenterPosition(center.getGeoPosition());
+				
+				Set<GeoPosition> boundSet = new HashSet<GeoPosition>(2);
+				boundSet.add(bounds.max.getGeoPosition());
+				boundSet.add(bounds.min.getGeoPosition());
+				mapComponent.setZoom(mapComponent.getTileFactory().getInfo().getMinimumZoomLevel());
+				mapComponent.calculateZoomFrom(boundSet);
+			}
+			catch (ReadException exception)
+			{
+				JOptionPane.showMessageDialog(null, exception.getMessage(), "Read error", JOptionPane.ERROR_MESSAGE);
+			}
+			return null;
+		}
+		
+		@Override
+		protected void done()
+		{
+			mapComponent.removeOverlayPainter(infoTextOverlay);
+		}
+	}
 	
 	public OpenAction(MapComponent mapComponent)
 	{
@@ -60,29 +108,16 @@ public class OpenAction extends ApplicationAction
 		{
 			File file = fc.getSelectedFile();
 			
-			GPXReader reader;
-			try
-			{
-				reader = new GPXReader(file);
-				GPXData data = reader.getGPXDataObject();
-				
-				mapComponent.addOverlayPainter(new GPXPainter(data).getPainter());
-				// TODO: Remove debug information
-				System.out.println(data.toString());
-				
-				Bounds bounds = data.getBounds();
-				LatLon center = bounds.getCenter();
-				mapComponent.setCenterPosition(center.getGeoPosition());
-				
-				Set<GeoPosition> boundSet = new HashSet<GeoPosition>(2);
-				boundSet.add(bounds.max.getGeoPosition());
-				boundSet.add(bounds.min.getGeoPosition());
-				mapComponent.setZoom(mapComponent.getTileFactory().getInfo().getMinimumZoomLevel());
-				mapComponent.calculateZoomFrom(boundSet);
-			}
-			catch (ReadException exception)
-			{
-				JOptionPane.showMessageDialog(null, exception.getMessage(), "Read error", JOptionPane.ERROR_MESSAGE);
+			String infoText = "Loading file. Please wait...";
+			InformativeTextPainter loadingInfoTextOverlay = new InformativeTextPainter(mapComponent, infoText);
+			
+			mapComponent.addOverlayPainter(loadingInfoTextOverlay.getPainter());
+			
+			try {
+				new OpenActivity(file, loadingInfoTextOverlay.getPainter()).execute();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		}
 	}
