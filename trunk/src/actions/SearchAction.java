bacbd7254a54a6ce3e8416a2f181f8f05e6d53ec
mschaeuble@swissonline.ch <mschaeuble@swissonline.ch@a0df532e-5e5b-11de-8a4b-256bef446c6c>
2009-08-26 10:01:01 +0000

    Added search action

---
+++ trunk/src/actions/SearchAction.java
@@ -0,0 +1,54 @@
+package actions;
+
+import gui.SearchComponent;
+
+import java.awt.event.ActionEvent;
+
+import org.geonames.Toponym;
+import org.geonames.ToponymSearchCriteria;
+import org.geonames.ToponymSearchResult;
+import org.geonames.WebService;
+
+public class SearchAction extends ApplicationAction
+{	
+	private SearchComponent searchComponent;
+	
+	public SearchAction(SearchComponent searchComponent)
+	{
+		super("Search", "search", 0, 0);
+		
+		this.searchComponent = searchComponent;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void actionPerformed(ActionEvent event)
+	{
+		String searchString = searchComponent.getSearchString().trim();
+		
+		if(searchString.length() > 0)
+		{
+			searchComponent.setStatus(SearchComponent.Status.SEARCHING);
+			ToponymSearchCriteria searchCriteria = new ToponymSearchCriteria();
+			searchCriteria.setQ(searchString);
+			ToponymSearchResult searchResult = null;
+			try
+			{
+				searchResult = WebService.search(searchCriteria);
+			}
+			catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			for (Toponym toponym : searchResult.getToponyms())
+			{
+				System.out.println(toponym.getName()+" "+ toponym.getCountryName());
+			}
+			System.out.println("_-------------------------------");
+			searchComponent.setStatus(SearchComponent.Status.READY);
+		}
+	}
+
+}
