92fdfcf307a479819fd30451d72454fa91865e08
mschaeuble <mschaeuble@swissonline.ch>
2009-08-27 08:13:14 +0000

    Continued implementation of save map extract action.

--- src/actions/SaveMapExtract.java
+++ src/actions/SaveMapExtract.java
@@ -2,8 +2,18 @@ package actions;
 
 import gui.MapComponent;
 
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class SaveMapExtract extends ApplicationAction
 {
@@ -19,19 +29,33 @@ public class SaveMapExtract extends ApplicationAction
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("hello");
-//		BufferedImage image;
-//        Robot robot = null;
-//        try {
-//            robot = new Robot();
-//        } catch (AWTException ex) {
-//            ex.printStackTrace();
-//        }
-//        Rectangle viewRect = jScrollPane1.getViewport().getViewRect();
-//        Point componentLocation =
-//                jScrollPane1.getViewport().getView().getLocationOnScreen();
-//        viewRect.translate(componentLocation.x, componentLocation.y);
-//        image = robot.createScreenCapture(viewRect);
+        Robot robot = null;
+		try
+		{
+			robot = new Robot();
+		}
+		catch (AWTException e1)
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			System.exit(-1);
+		}
+        
+        Point componentLocation = mapComponent.getLocationOnScreen();
+        Dimension dimension = new Dimension(mapComponent.getWidth(), mapComponent.getHeight());
+        Rectangle rectangle = new Rectangle(componentLocation, dimension);
+        BufferedImage image = robot.createScreenCapture(rectangle);
+
+        File outputfile = new File("saved.png");
+        try
+        {
+			ImageIO.write(image, "png", outputfile);
+		}
+        catch (IOException e1)
+        {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 	}
 
